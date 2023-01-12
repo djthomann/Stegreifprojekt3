42088901d10271795b4b005c6cf559b74208c458
THO <THO@DESKTOP-A34U6OB>
2023-01-12 02:41:14 +0100

    Tenth Commit
    Adjusted JFrame to be centered, non-resizable and Instanciate with two
    variables for width and height

--- Stegreifprojekt3_Die_Häuser_von_Hogsmeade/src/HogsmeadeApp.java
+++ Stegreifprojekt3_Die_Häuser_von_Hogsmeade/src/HogsmeadeApp.java
@@ -3,7 +3,7 @@ import javax.swing.JFrame;
 /**
  * Starter der Hogsmaede-Applikation.
  * 
- * Applikation wird in einem Standard-Fenster der Groesse 700 x 500 angezeigt.
+ * Applikation wird in einem Standard-Fenster der Groesse 700 x 500 angezeigt. --> Abgeändert
  *
  * @author berdux
  *
@@ -14,7 +14,7 @@ public class HogsmeadeApp {
 	 * 
 	 * @param args wird nicht interpretiert
 	 */
-	private final static int WIDTH = 700;
+	private final static int WIDTH = 900;
 	private final static int HEIGHT = 500;
 	
 	// Getter und Setter
@@ -30,7 +30,10 @@ public class HogsmeadeApp {
 		// Initialisierung des Frames
         JFrame jFrame = new JFrame();
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jFrame.setSize(700, 500);
+        jFrame.setSize(WIDTH, HEIGHT);
+        jFrame.setResizable(false);
+        jFrame.setLocationRelativeTo(null);
+        
         
         /* Instanziierung der eigentlichen Anzeige, 
          * die in in der Klasse Hogsmaede definiert ist.
